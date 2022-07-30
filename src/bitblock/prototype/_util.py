8c4fb4c3cefbc354ae4f3d59e2be8cfbad7f3ff4
Charlotte Lockett <103963818+char-lock@users.noreply.github.com>
2022-07-30 16:28:29 -0400

    Correct endianness for packing
    I noticed that my current architecture uses big endian notation as opposed to Bitcoin's use of little endian in most things. I'll add a cleaner swap than is currently in here, but there's a rough fix right now.
    
    The current issue I'm running into is with parsing transactions. I'll have to figure that out later.

--- src/bitblock/prototype/_util.py
+++ src/bitblock/prototype/_util.py
@@ -4,14 +4,14 @@ Useful helper functions that can be used in multiple parts of
 prototype modules.
 
 """
-from io import BufferedReader
+from io import BufferedReader, BytesIO
 from numpy import (
     uint8, int8,
     uint16, int16,
     uint32, int32,
     uint64, int64,
 )
-from typing import Literal, Optional, TypeAlias, Union
+from typing import Literal, Optional, TypeAlias, Union, List, Dict
 
 int_t: TypeAlias = Union[
     uint8, int8,
@@ -51,9 +51,9 @@ def pack_bytes(
         raise OverflowError("Provided `x` is larger than requested `bit_sz`.")
     for _b in range(_byte_sz):
         _shift: int = 8 * (_byte_sz - ((_b + 1) + (_byte_sz - _x_sz)))
-        if _shift < 0:
+        if _shift < 0 or _shift > bit_sz - 8:
             print(f"{_byte_sz} - (({_b} + 1) + ({_byte_sz} - {_x_sz}))")
-        _v: int = x[_b] if _b < _x_sz else 0x00
+        _v: int = x[_x_sz - 1 - _b] if _b <= _x_sz else 0x00
         _value |= _v << _shift
     if signed:
         if bit_sz == 8:
@@ -92,16 +92,90 @@ def read_var_int(block: BufferedReader) -> int_t:
     _disc: uint8 = pack_bytes(block.read(1), 8, False)
     if _disc == uint8(0xff):
         _value: uint64 = pack_bytes(block.read(8), 64, False)
-        if _value < uint64(0x100000000):
+        if int(_value.byteswap()) < int(0x100000000):
             raise ValueError("Not canonical. Encoding too large. 64-bit used.")
     elif _disc == uint8(0xfe):
         _value: uint32 = pack_bytes(block.read(4), 32, False)
-        if _value < uint32(0x10000):
+        if _value.byteswap() < int(0x10000):
             raise ValueError("Not canonical. Encoding too large. 32-bit used.")
     elif _disc == uint8(0xfd):
         _value: uint16 = pack_bytes(block.read(2), 16, False)
-        if _value < uint16(0x100):
+        if _value < int(0xfd):
             raise ValueError("Not canonical. Encoding too large. 16-bit used.")
     else:
         _value: uint8 = _disc
     return _value
+
+def decode_tx(block: BufferedReader) -> Dict:
+    """ Reads transactions from a BufferedReader.
+
+    ### Parameters
+    --------------
+
+    block: BufferedReader
+        where to read tx from
+    
+    tx_sz: int
+        how many tx to read
+
+    """
+    _version: uint32 = pack_bytes(block.read(4), 32, False)
+    _tx_in_sz: int_t = read_var_int(block)
+    print(_tx_in_sz)
+    _tx_in: List = list()
+    for _i in range(_tx_in_sz):
+        _hash: str = endian_swap(block.read(32)).hex()
+        _index: uint32 = pack_bytes(block.read(4), 32, False)
+        _script_sz: int_t = read_var_int(block)
+        _sig_script_buffer: List = list()
+        _script_sz_mut = int(_script_sz)
+        print(_script_sz_mut)
+        while _script_sz_mut > 0:
+            if _script_sz_mut >= 0xffffffff:
+                _script_sz_mut -= 0xffffffff
+                _sig_script_buffer.append(block.read(0xffffffff).hex())
+            else:
+                _sig_script_buffer.append(block.read(_script_sz_mut).hex())
+                _script_sz_mut = 0
+        _sig_script = ''.join(_sig_script_buffer)
+        _sequence: uint32 = pack_bytes(block.read(4), 32, False)
+        _tx_in.append([_hash, _index, _script_sz, _sig_script, _sequence])
+    _tx_out_sz: int_t = read_var_int(block)
+    _tx_out: List = list()
+    for _i in range(_tx_out_sz):
+        _value: uint64 = pack_bytes(block.read(8), 64, False)
+        _script_sz: int_t = read_var_int(block)
+        _pk_script_buffer: List = list()
+        _script_sz_mut = int(_script_sz)
+        while _script_sz_mut > 0:
+            if _script_sz_mut >= 0xffffffff:
+                _script_sz_mut -= 0xffffffff
+                _pk_script_buffer.append(block.read(0xffffffff).hex())
+            else:
+                _pk_script_buffer.append(block.read(_script_sz_mut).hex())
+                _script_sz_mut = 0
+        _pk_script = ''.join(_pk_script_buffer)
+        _tx_out.append([_value, _script_sz, _pk_script])
+    _lock_time: uint32 = pack_bytes(block.read(4), 32, False)
+    return {
+        "version": _version,
+        "tx_in_sz": _tx_in_sz,
+        "tx_in": _tx_in,
+        "tx_out_sz": _tx_out_sz,
+        "tx_out": _tx_out,
+        "lockTime": _lock_time
+    }
+
+def read_all_tx(block: BufferedReader, tx_sz: int_t) -> List:
+    """ Reads and returns a list of all transactions in the block. """
+    _tx: List = list()
+    for _i in range(tx_sz):
+        print(_i)
+        _tx.append(decode_tx(block))
+    return _tx
+
+def endian_swap(b: bytes) -> bytes:
+    _new = list()
+    for i in range(len(b)):
+        _new.append(b[len(b) - 1 - i])
+    return bytes(_new)
--- src/bitblock/prototype/blockreader.py
+++ src/bitblock/prototype/blockreader.py
@@ -4,7 +4,7 @@ A module to parse raw block files.
 
 """
 from numpy import int32, uint32, uint64
-from typing import TypeAlias, List
+from typing import Dict, TypeAlias, List
 from io import BufferedReader, FileIO, BytesIO
 from _util import *
 from guppy import hpy
@@ -13,84 +13,56 @@ from hashlib import sha256
 hp = hpy()
 
 BLOCK_DIR = r"E:\Bitcoin\blockchain\blocks"
-MAGIC = uint32(0xf9beb4d9)
+MAGIC = uint32(0xd9b4bef9)
 
-def open_block(blk):
-    # Read from the requested blk file.
-    _blk_dir = BLOCK_DIR + '\\' + blk + '.dat'
-    _blk_ = FileIO(_blk_dir, mode="r")
-    _blk  = BufferedReader(_blk_)
-    # Check the first 32-bit value against the known validation value.
-    _magic: uint32 = pack_bytes(_blk.read(4), 32, False)
-    if int(_magic) != int(MAGIC):
-        raise Exception("Incorrect start.")
-    # Pull the first block from the file.
-    _block_sz: uint32 = pack_bytes(_blk.read(4), 32, False)
-    _block_: BytesIO = BytesIO(_blk.read(_block_sz))
-    _block: BufferedReader = BufferedReader(_block_, _block_sz)
-    # Process the block header.
-    _version: uint32 = pack_bytes(_block.read(4), 32, False)
-    _prev_block: str = _block.read(32).hex()
-    _merkle_root: str = _block.read(32).hex()
-    _time: int64 = int64(pack_bytes(_block.read(4), 32, False))
-    _bits: uint32 = pack_bytes(_block.read(4), 32, False)
-    _nonce: uint32 = pack_bytes(_block.read(4), 32, False)
-    _block.seek(0, 0)
-    _hash: str = sha256(sha256(_block.read(80)).digest()).hexdigest()
-    print(f"""Hash: {_hash}
-Version: {_version}
-Previous Block: {_prev_block}
-Merkle Root: {_merkle_root}
-Time: {_time}
-Bits: {_bits}
-Nonce: {_nonce}""")
-    # Process transactions
-    _tx_sz: int_t = read_var_int(_block)
-    print(f"Tx Count: {_tx_sz}")
-    _tx: List = read_all_tx(_block, _tx_sz)
-    print(f"Tx Length: {len(_tx)}")
-
-
-def decode_tx(block: BufferedReader) -> List:
-    """ Reads transactions from a BufferedReader.
-
-    ### Parameters
-    --------------
-
-    block: BufferedReader
-        where to read tx from
+class BlockCache(object):
+    """ A block cache file (blkxxxxx.dat) containing saved block info. """
+    def __init__(self, blk: int):
+        self._file_id: int = blk
+        self._filename: str = f'blk{str(blk).zfill(5)}.dat'
+        self._dir:str = f'{BLOCK_DIR}\\'
+        self._file = FileIO(f'{self._dir}{self._filename}', mode="r")
+        self._reader = BufferedReader(self._file)
+        self._reader.seek(0, 2)
+        self._eof = self._reader.tell()
+        self._reader.seek(0, 0)
+        self._blocks = list()
+        self._read_all_blocks()
+        print(len(self._blocks))
     
-    tx_sz: int
-        how many tx to read
+    def _check_file(self) -> None:
+        _magic: uint32 = pack_bytes(self._read(4), 32, False)
+        if int(_magic) != int(MAGIC):
+            raise Exception("Incorrect start value.")
 
-    """
-    _version: uint32 = pack_bytes(block.read(4), 32, False)
-    _tx_in_sz: int_t = read_var_int(block)
-    _tx_in: List = list()
-    for _i in range(_tx_in_sz):
-        _hash: str = block.read(32).hex()
-        _index: uint32 = pack_bytes(block.read(4), 32, False)
-        _script_sz: int_t = read_var_int(block)
-        _sig_script: str = block.read(_script_sz).hex()
-        _sequence: uint32 = pack_bytes(block.read(4), 32, False)
-        _tx_in.append([_hash, _index, _script_sz, _sig_script, _sequence])
-    _tx_out_sz: int_t = read_var_int(block)
-    _tx_out: List = list()
-    for _i in range(_tx_out_sz):
-        _value: uint64 = pack_bytes(block.read(8), 64, False)
-        _script_sz: int_t = read_var_int(block)
-        _pk_script: str = block.read(_script_sz).hex()
-        _tx_out.append([_value, _script_sz, _pk_script])
-    _lock_time: uint32 = pack_bytes(block.read(4), 32, False)
-    return [_version, _tx_in_sz, _tx_in, _tx_out_sz, _tx_out, _lock_time]
+    def _read_block(self) -> Dict:
+        self._check_file()
+        _block = {}
+        _block["start_pos"]: int = self._reader.tell()
+        _block["sz"]: uint32 = pack_bytes(self._read(4), 32, False)
+        _cache_ = BytesIO(self._read(_block["sz"]))
+        _cache = BufferedReader(_cache_, _block["sz"])
+        _block["version"]: uint32 = pack_bytes(_cache.read(4), 32, False)
+        _block["prevBlock"]: str = endian_swap(_cache.read(32)).hex()
+        _block["merkleRoot"]: str = endian_swap(_cache.read(32)).hex()
+        _block["time"]: int64 = int64(pack_bytes(_cache.read(4), 32, False))
+        _block["bits"]: uint32 = pack_bytes(_cache.read(4), 32, False)
+        _block["nonce"]: uint32 = pack_bytes(_cache.read(4), 32, False)
+        _cache.seek(0, 0)
+        _block["hash"]: str = endian_swap(sha256(
+            sha256(_cache.read(80)).digest()
+        ).digest()).hex()
+        _block["tx_sz"] = read_var_int(_cache)
+        print(_block["hash"] + ": " + str(_block["tx_sz"]))
+        _block["tx"] = read_all_tx(_cache, _block["tx_sz"])
+        _block["end_pos"]: int = self._reader.tell()
+        return _block
 
+    def _read_all_blocks(self) -> List:
+        while self._reader.tell() < self._eof:
+            self._blocks.append(self._read_block())
 
-def read_all_tx(block: BufferedReader, tx_sz: int_t) -> List:
-    """ Reads and returns a list of all transactions in the block. """
-    _tx: List = list()
-    for _i in range(tx_sz):
-        _tx.append(decode_tx(block))
-    return _tx
+    def _read(self, sz: int) -> bytes:
+        return self._reader.read(sz)
 
-
-open_block('blk02422')
+test = BlockCache(2211)
